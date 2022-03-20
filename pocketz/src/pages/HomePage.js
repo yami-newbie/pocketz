29468a27f0db1eea81b7e0032f02d5a1f069e377
khanhtoan122001 <khanhtoannguyen12@gmail.com>
2022-03-21 00:52:19 +0700

    update gì đó k nhớ

--- pocketz/src/pages/HomePage.js
+++ pocketz/src/pages/HomePage.js
@@ -2,20 +2,32 @@ import { useAuth } from "../serviceData/walletAccount";
 import {useNavigate} from 'react-router-dom'
 import MainLayout from "../components/MainLayout";
 import { useListAccount } from "../serviceData/listAccount";
+import { useEffect } from "react";
 
 function HomePage() {
   let navigate = useNavigate();
     const auth = useAuth();
     const listAccount = useListAccount();
+    useEffect(() => {
+      const init = () => {
+        console.log("auth.wallet.password", auth.wallet.password);
+        console.log("auth.wallet.isLogin", auth.wallet.isLogin);
+        if(!auth.wallet.password) {
+          console.log("nah");
+          return navigate("./register")
+        }
+        if(!auth.wallet.isLogin) {
+          console.log("nah2");
+          return navigate("./login")
+        }
+      }
+      init();
+    }, [])
     return (
       <div>
-        {auth.wallet.isLogin ? (
-          <div>
-            <MainLayout Account={listAccount.getSelectedAccount()} />
-          </div>
-        ) : (
-          navigate("./login")
-        )}
+        <div>
+          <MainLayout Account={listAccount.getSelectedAccount()} />
+        </div>
       </div>
     );
 }
