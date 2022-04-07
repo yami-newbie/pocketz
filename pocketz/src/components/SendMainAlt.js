3866a71e59a6c1616835cb0c2395620e6bffada6
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2022-04-07 15:07:27 +0700

    s-update
    cập nhật lại giao diện gửi token

--- pocketz/src/components/SendMainAlt.js
+++ pocketz/src/components/SendMainAlt.js
@@ -1,11 +1,10 @@
 import { Card, IconButton, Select, Typography, MenuItem, TextField, Button, Stack, CardContent } from '@mui/material'
-import { Box } from '@mui/system'
 import React from 'react'
 import { useListAccount } from '../serviceData/listAccount'
 import ClearIcon from '@mui/icons-material/Clear';
 import { useNavigate } from 'react-router';
 
-export default function SendMainAlt() {
+export default function SendMainAlt({Account, onExit}) {
   const listAcc = useListAccount();
   const acc = listAcc.getSelectedAccount();
   let navigate = useNavigate();
@@ -17,11 +16,11 @@ export default function SendMainAlt() {
     return String(address).substring(0,10);
   }
   return (
-    <div className="centered-container" sx={{ width: '360px' }}>
-      <Card sx={{ width: '360px' }} >
+    <div className="centered-container" sx={{ width: "360px" }}>
+      <Card sx={{ width: "360px" }}>
         <div>
-          <div className='kbietdattengi'>
-            <div className='margin-left'>
+          <div className="kbietdattengi">
+            <div className="margin-left">
               <Typography variant="body1" gutterBottom>
                 {acc.username}
               </Typography>
@@ -30,14 +29,18 @@ export default function SendMainAlt() {
               </Typography>
             </div>
             <div className="margin-right">
-              <IconButton onClick={()=>{navigate("/")}}>
+              <IconButton
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
                 <ClearIcon />
               </IconButton>
             </div>
           </div>
         </div>
-        <div className='item'>
-          <div className='margin-left'>
+        <div className="item">
+          <div className="margin-left">
             <Typography variant="body1" gutterBottom>
               Asset:
             </Typography>
@@ -46,37 +49,59 @@ export default function SendMainAlt() {
                 value={money}
                 onChange={handleChange}
                 displayEmpty
-                sx={{ width: '120px' }}
+                sx={{ width: "120px" }}
                 defaultValue={1}
               >
                 <MenuItem value={1}>ETH</MenuItem>
               </Select>
-              <Typography variant="caption" gutterBottom sx={{ padding: '10px' }}>
+              <Typography
+                variant="caption"
+                gutterBottom
+                sx={{ padding: "10px" }}
+              >
                 Balance: 0
               </Typography>
             </div>
           </div>
         </div>
-        <div className='item'>
-          <div className='margin-left'>
+        <div className="item">
+          <div className="margin-left">
             <Typography variant="body1" gutterBottom>
               Amount:
             </Typography>
-            <TextField variant='outlined'
+            <TextField
+              variant="outlined"
               defaultValue="0"
               sx={{ width: "95%" }}
             />
           </div>
         </div>
-        
-        <div style={{ margin: "10px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+
+        <div
+          style={{
+            margin: "10px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Stack sx={{ width: "90%" }} direction="row" spacing={2}>
-            <Button sx={{ width: "50%", borderRadius: "100px" }} variant="outlined">Cancel</Button>
-            <Button sx={{ width: "50%", borderRadius: "100px" }} variant="contained">Continue</Button>
+            <Button
+              onClick={onExit}
+              sx={{ width: "50%", borderRadius: "100px" }}
+              variant="outlined"
+            >
+              Huỷ
+            </Button>
+            <Button
+              sx={{ width: "50%", borderRadius: "100px" }}
+              variant="contained"
+            >
+              Tiếp tục
+            </Button>
           </Stack>
         </div>
-
       </Card>
     </div>
-  )
+  );
 }
--- pocketz/src/pages/SendTxPage.js
+++ pocketz/src/pages/SendTxPage.js
@@ -1,6 +1,4 @@
-import SendMainAlt from "../components/SendMainAlt";
 import SendMain from "../components/SendMain";
-import SendHeader from "../components/SendHeader";
 import { Card } from "@mui/material";
 
 function SendTransactionPage() {
@@ -8,9 +6,6 @@ function SendTransactionPage() {
     return (
         <div className="centered-container">
             <Card sx={{maxWidth: '360px'}}>
-                <div sx = {{height: '100px'}}>
-                    <SendHeader/>
-                </div>
                 <SendMain/>
             </Card>
         </div>
